3c2599fa92356b0088d2da86427d5b038352ebf5
Vlad <vl.lavrynovych@gmail.com>
2023-11-03 00:01:08 +0200

    Final revision

--- src/main/java/com/example/prometheus/PrometheusApplication.java
+++ src/main/java/com/example/prometheus/PrometheusApplication.java
@@ -1,13 +1,24 @@
 package com.example.prometheus;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class PrometheusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrometheusApplication.class, args);
 	}
 
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
 }
